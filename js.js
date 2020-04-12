616c7b5f4a49cca85a27153ca9b34b8e54146339
Fahim Shariff <52634330+Fahim-at-work@users.noreply.github.com>
2020-04-12 18:31:37 -0400

    initial commit

---
+++ js.js
@@ -0,0 +1,10 @@
+const view_rules_btn = document.getElementById('view-rules');
+view_rules_btn.addEventListener('click', viewRules);
+
+const close_rules_btn = document.getElementById('close-rules');
+close_rules_btn.addEventListener('click', closeRules)
+
+
+function viewRules() {
+
+}
